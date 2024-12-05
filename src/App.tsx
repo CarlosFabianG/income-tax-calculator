1c93180e6a1e81d4bf31459935a99cd156decfcf
CarlosFabianG <carlos.fabian@imaginelearning.com>
2024-12-05 08:19:12 -0600

    feat: completed app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,65 @@
-import { useState, useEffect } from 'react';
-import Form from './components/form';
-
+import { useState } from 'react';
+import Form from './components/Form';
+import Results from './components/Results';
+import { calculateTaxDetails } from './helpers/calculateTaxDetails';
+import { API_URL } from './config';
 import './App.css';
 
-function App() {
-  const [ error, setError ] = useState<string | null>(null);
+interface TaxDetails {
+  totalTax: string;
+  marginalRate: number;
+  effectiveTaxRate: string;
+  bracketDetails: {
+    min: number;
+    max: number;
+    rate: number;
+    taxableAmount: number;
+    bracketTax: number;
+  }[];
+}
 
-  const onSubmit = () => {
-    console.log('Submit!!!!');
+const TAX_CALCULATOR_URL = `${API_URL}/tax-calculator/tax-year/`
+
+function App() {
+  const [ error, setError ] = useState(false);
+  const [ isLoading, setIsLoading ] = useState(false);
+  const [calculationResults, setCalculationResults] = useState<TaxDetails | null>({} as TaxDetails);
+
+  const onSubmit = async (income: string, year: string) => {
+    try {
+      setIsLoading(true);
+      const response = await fetch(TAX_CALCULATOR_URL + year);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      const { tax_brackets } = data;
+      const taxDetails = await calculateTaxDetails(income, tax_brackets);
+      setCalculationResults(taxDetails);
+      setIsLoading(false);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      setIsLoading(false);
+      setError(true);
+    }
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>
-          Tax Annual Income
+          Income Tax Calculator
         </h1>
       </header>
-      {error && <p>This is and error!!</p>}
-      <Form onSubmit={onSubmit}/>
-      <footer>Made with ❤️ in Mexico</footer>
+      <section>
+        <Form submit={onSubmit} error={error} setError={setError} />
+        {error || !calculationResults ? <Results error={error} isLoading={isLoading} /> 
+        : 
+        <Results isLoading={isLoading} calculationResults={calculationResults}/>}
+      </section>
+      <footer>Made with ❤️ in Mexico by <b><a href='https://www.carlosfabian.dev/about'>Carlos Fabian</a></b></footer>
     </div>
   );
-};
+}
 
 export default App;
